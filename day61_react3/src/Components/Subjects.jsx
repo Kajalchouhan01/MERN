1befca1a34feb1b21d0c852159d940ced6c8196a
Kajalchouhan01 <kajalchouhan37@gmail.com>
2024-06-07 16:49:57 +0530

    css jsx

---
+++ day61_react3/src/Components/Subjects.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Subjects = () => {
+    const myStyle ={backgroundColor:'red',
+        color:'white'}
+  return (
+    <>
+    <div style={{backgroundColor:'blue'
+        ,padding:'10px'
+,textAlign:'center'    }}>MERN
+<h2 >react</h2>
+<h2>java</h2>
+<h2>html</h2>
+
+
+    </div>
+    <div style={myStyle}>we are learning</div>
+
+    </>
+  );
+}
+
+export default Subjects
---
+++ day61_react3/src/Components/Superman.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './product.css'
+
+const Superman = () => {
+  return (
+    <div className='product'>this is superman</div>
+  )
+}
+
+export default Superman
